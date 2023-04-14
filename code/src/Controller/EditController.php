6a57de048cd6f4446348a7afcf2cdb5589bf3c45
Kplukas <p.kristupas.p@gmail.com>
2023-04-14 11:23:48 +0300

    Adding edit page (just the route)

---
+++ code/src/Controller/EditController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Article;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class EditController extends AbstractController
+{
+    #[Route('/edit/{id}', name: 'article_edit')]
+    public function view(Article $article): Response
+    {
+        return $this->render('pages/edit.html.twig', [
+            'article' => $article,
+        ]);
+    }
+}
